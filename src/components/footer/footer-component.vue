4ef3edb1553280a10546218737b510d1a4cf4b5d
wahyou <wahyukrisnaaji32@gmail.com>
2022-12-27 16:36:37 +0700

    finished dashboard component

---
+++ src/components/footer/footer-component.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-row>
+        <v-col cols="9">
+            <v-card-text class="text-caption">
+                Make with <v-icon>mdi-heart</v-icon> by <b>Swing Developer</b>
+            </v-card-text>
+        </v-col>
+
+        <v-col cols="3">
+            <v-card-text>
+                &copy; Swing 2023
+            </v-card-text>
+        </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/dashboard-view.vue
+++ src/views/dashboard-view.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="main-bg">
     <v-container>
       <titleBar class="mb"></titleBar>
       <v-row>
@@ -25,6 +25,22 @@
           <popularProductComponentVue></popularProductComponentVue>
         </v-col>
       </v-row>
+
+      <v-row>
+        <v-col cols="6">
+          <trafficComponentVue></trafficComponentVue>
+        </v-col>
+
+        <v-col cols="6">
+          <activityComponent></activityComponent>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <footerComponent></footerComponent>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -34,12 +50,18 @@ import titleBar from '@/components/dashboard/title-bar-component.vue';
 import panelComponentVue from '@/components/dashboard/panel-component.vue';
 import chartComponentVue from '@/components/dashboard/chart-component.vue';
 import popularProductComponentVue from '@/components/dashboard/popular-product-component.vue';
+import trafficComponentVue from '@/components/dashboard/traffic-component.vue';
+import activityComponent from '@/components/dashboard/activity-component.vue';
+import footerComponent from '@/components/footer/footer-component.vue';
 export default {
   components: {
     titleBar,
     panelComponentVue,
     chartComponentVue,
-    popularProductComponentVue
+    popularProductComponentVue,
+    trafficComponentVue,
+    activityComponent,
+    footerComponent
   }
 }
 </script>
@@ -47,5 +69,8 @@ export default {
 <style scoped>
   .mb {
     margin-bottom: 15px;
+  }
+  .main-bg {
+    background-color: rgb(227, 227, 221);
   }
 </style>
