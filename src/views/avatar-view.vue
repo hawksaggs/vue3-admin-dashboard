a09f943cc91e10948a500e555341595cf97a010a
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2022-12-31 11:50:42 +0700

    update avatar code and change README file to READMEVUE

--- src/views/avatar-view.vue
+++ src/views/avatar-view.vue
@@ -1,45 +1,40 @@
 <template>
   <div class="main-bg">
     <v-container>
-        <titlebarAvatarComponentVue class="mb"></titlebarAvatarComponentVue>
-        <avatarComponent class="mb"></avatarComponent>
-        <avatarInitial></avatarInitial>
+      <titlebarAvatarComponentVue class="mb"></titlebarAvatarComponentVue>
+      <avatarComponent class="mb"></avatarComponent>
+      <avatarInitial class="mb"></avatarInitial>
 
-        <v-row>
-            <v-col cols="12">
-                <footerComponentVue class="bottom"></footerComponentVue>
-            </v-col>
-        </v-row>
+      <v-row>
+        <v-col cols="12">
+          <footerComponentVue></footerComponentVue>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
-import titlebarAvatarComponentVue from '@/components/avatar/titlebar-avatar-component.vue';
-import avatarComponent from '@/components/avatar/avatar-component.vue';
-import avatarInitial from '@/components/avatar/avatar-initial-component.vue';
-import footerComponentVue from '@/components/footer/footer-component.vue';
+import titlebarAvatarComponentVue from "@/components/avatar/titlebar-avatar-component.vue";
+import avatarComponent from "@/components/avatar/avatar-component.vue";
+import avatarInitial from "@/components/avatar/avatar-initial-component.vue";
+import footerComponentVue from "@/components/footer/footer-component.vue";
 export default {
-    components: {
-        titlebarAvatarComponentVue,
-        avatarComponent,
-        avatarInitial,
-        footerComponentVue
-    }
-}
+  components: {
+    titlebarAvatarComponentVue,
+    avatarComponent,
+    avatarInitial,
+    footerComponentVue,
+  },
+};
 </script>
 
 <style scoped>
-  .mb {
-    margin-bottom: 15px;
-  }
-  .main-bg {
-    background-color: rgb(227, 227, 221);
-    height: 100%;
-  }
-  .bottom {
-    position: absolute;
-    bottom: 0;
-    width: 70%;
-  }
+.mb {
+  margin-bottom: 15px;
+}
+.main-bg {
+  background-color: rgb(227, 227, 221);
+  height: 100%;
+}
 </style>
