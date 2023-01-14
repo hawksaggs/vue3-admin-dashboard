2ed4cdeed2e7de768f6cdb98c5940fb916ff7afb
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2023-01-14 11:57:04 +0700

    fix some components

--- src/views/top-nav-view.vue
+++ src/views/top-nav-view.vue
@@ -4,23 +4,15 @@
       <topNavigationComponentVue></topNavigationComponentVue>
 
       <style1></style1>
-
-      <v-row>
-        <v-col cols="cols">
-          <footerComponentVue></footerComponentVue>
-        </v-col>
-      </v-row>
     </v-container>
   </div>
 </template>
 
 <script>
 import topNavigationComponentVue from "@/components/top-nav/top-navigation-component.vue";
-import footerComponentVue from "@/components/footer/footer-component.vue";
 import style1 from "@/components/top-nav/style1-component.vue";
 export default {
   components: {
-    footerComponentVue,
     topNavigationComponentVue,
     style1,
   },
